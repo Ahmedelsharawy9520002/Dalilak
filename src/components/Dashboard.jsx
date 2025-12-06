26db426b728e20ee4eb4b857f9da79b5a40e2036
Ahmedelsharawy9520002 <ahmed9520002@gmail.com>
2025-12-06 23:15:54 +0200

    dasboard routes (exlopre more_roadmaps, continue_roadmaps)

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,8 +2,8 @@ import "../styles/dashboard.css";
 import startImg from "../images/start.png";
 import progressImg from "../images/progress.png";
 import completedImg from "../images/complete.png";
-
-
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 function Dashboard() {
   return (
     <div className="dashboard-container">
@@ -19,9 +19,11 @@ function Dashboard() {
             </div>
         
             <div>
+              <Link to="/Roadmaps">
                 <button className="explore-btn px-4 py-2 mt-3">
                     Explore More Roadmaps {<svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right w-4 h-4"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg>}
                 </button>
+              </Link>
             </div>
           </div>
        </div>
@@ -117,10 +119,12 @@ function Dashboard() {
         <svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="endlogo lucide lucide-zap w-12 h-12 mx-auto mb-4 text-accent"><path d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z"></path></svg>
         <h4>Keep the momentum going</h4>
         <p>Continue with your next milestone or explore a new roadmap</p>
+        <Link to="/Roadmaps">
         <button className="explore-btn learn-btn px-4 py-2">
             Continue Learning 
             {<svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right w-4 h-4"><path d="M5 12h14"></path><path d="m12 5 7 7-7 7"></path></svg>}
          </button>
+         </Link>
       </div>
 
     </div>

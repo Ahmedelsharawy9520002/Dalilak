2ebe7561151e21de59b322638a95b75d0233f4bc
Ahmedelsharawy9520002 <ahmed9520002@gmail.com>
2025-12-14 17:51:59 +0200

    animations updates

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -3,10 +3,10 @@ import { motion } from "framer-motion";
 function PageWrapper({ children }) {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: 10}}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
-      transition={{ duration: 0.4 }}
+      transition={{ duration: 0.6  , ease: "easeOut" }}
     >
       {children}
     </motion.div>
